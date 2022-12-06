d24c6e21df21f5dbc7b86d3236ab88bb6dd1329a
thevoidop <119924028+thevoidop@users.noreply.github.com>
2022-12-06 19:28:50 +0530

    Add files via upload
    the game doesn't crash even if you enter alphabets

--- the_guess_game.py
+++ the_guess_game.py
@@ -5,21 +5,25 @@ print("\t\tComputer is choosing a random number\n")
 gusses = 0
 userInput = None
 
-while(userInput != randNum):
-    userInput = int(input("Guess the number: "))
-    gusses += 1
-    if (userInput == randNum):
-        print("\nCongratulations! You guessed it right.")
-        print(f"It took you {gusses} gusses to win.")
-    else :
-        if (userInput < randNum):
-            print("You gussed it wrong! Choose a HIGHER numer.\n")
-        else :
-            print("You gussed it wrong! Choose a LOWER numer.\n")
+while (True):
+        while(userInput != randNum):
+            try:
+                userInput = int(input("Guess the number: "))
+                gusses += 1
+                if (userInput == randNum):
+                    print("\nCongratulations! You guessed it right.")
+                    print(f"It took you {gusses} gusses to win.")
+                else :
+                    if (userInput < randNum):
+                        print("You gussed it wrong! Choose a HIGHER numer.\n")
+                    else :
+                        print("You gussed it wrong! Choose a LOWER numer.\n")
+            except Exception as e:
+                 print("Please enter a valid number")
+        break
     
 with open("game_hs.txt") as f:
     record = int(f.read())
 if (gusses < record):
     with open("game_hs.txt", "w") as f:
         f.write(str(gusses)) 
-    
